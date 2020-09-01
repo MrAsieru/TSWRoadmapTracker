a2ff8b5867b5e141259544063a134b7a7095e634
MrAsieru <asiescp@gmail.com>
2020-09-01 19:01:50 +0200

    Roadmap update 2020/09/01

--- jsonCreator.py
+++ jsonCreator.py
@@ -1,3 +1,4 @@
+#Python 3.8.3
 import re
 import json
 import urllib.request
@@ -6,8 +7,16 @@ from datetime import datetime as dt
 from shutil import copyfile
 
 DEBUG = False
+LOCAL_JSON = True
 URL = "https://raw.githubusercontent.com/MrAsieru/TSWRoadmapTracker/master/data.json"
-sections = ["Next Arrival","Upcoming","In Production","In Planning"]
+
+typesCodes = []
+statusCodes = []
+regionCodes = []
+tractionCodes = []
+addOnCodes = []
+yearsCodes = []
+
 newProjects = []
 oldProjects = []
 mergedProjects = []
@@ -51,12 +60,26 @@ def askDate():
     dtNow = str(dt.now()).replace(" ","").replace("-","").replace(":","").split(".")[0]
 
 def JSONtoDict():
+    global p_dict
     print("Downloading old data...")
-    f = urllib.request.urlopen(URL) #Download json data from GitHub
-    fD = "" #For decoded data
-    for line in f:
-        fD = fD + line.decode("utf-8")
-    p_dict = json.loads(fD)#Dictionary from json data
+    if not(LOCAL_JSON):
+        f = urllib.request.urlopen(URL) #Download json data from GitHub
+        fD = "" #For decoded data
+        for line in f:
+            fD = fD + line.decode("utf-8")
+        p_dict = json.loads(fD)#Dictionary from json data
+    else:
+        with open("dataTmp.json") as json_file:
+            p_dict = json.load(json_file)
+
+def getCodes():
+    global typesCodes,statusCodes,regionCodes,tractionCodes,addOnCodes,yearsCodes
+    typesCodes = p_dict["Codes"]["types"]
+    statusCodes = p_dict["Codes"]["status"]
+    regionCodes = p_dict["Codes"]["regions"]
+    tractionCodes = p_dict["Codes"]["tractions"]
+    addOnCodes = p_dict["Codes"]["addOnTypes"]
+    yearsCodes = p_dict["Codes"]["years"]
 
 def getRoadmapString():
     print("Creating file from pasted data...")
@@ -84,21 +107,40 @@ def createNewProjectsObjects():
     status = 0
     for i in file:
         i = i.replace("\n","") #Remove the line ending
-        if (i in sections): #Check if line is section title
+        if (i in statusCodes): #Check if line is section title
             status += 1
+        elif (i[0] != "["): #Check if new status
+            raise Exception("Posible new status found: %s"%i)
         else:
             TYPE = re.search("\[.*\]",i).group(0).replace("[","").replace("]","")
+            if not (TYPE in typesCodes):
+                raise Exception("Posible new type found: %s"%TYPE)
             try:
-                ID = re.search("\] .{4,}? -",i).group(0).replace("] ","").replace(" -","")
+                ID = re.search("[A-Z]{3}-[LR][1-7] (0[1-9]|[1-9][0-9])",i).group(0)
                 NAME = re.search("- .*$",i).group(0).replace("- ","")
             except AttributeError:
                 ID = re.search("] .*$",i).group(0).replace("] ","")
                 NAME = ""
-            STATUS = sections[status - 1]
+            STATUS = statusCodes[status - 1]
             newProj = project(ID,TYPE,NAME,STATUS,[date],[STATUS])
             newProjects.append(newProj)
     file.close
     print("Projects: %s"%len(newProjects))
+
+def checkIdCodes():
+    global newProjects,regionCodes,tractionCodes,addOnCodes,yearsCodes
+    for p in newProjects:
+        if re.match("[A-Z]{3}-[LR][1-7] (0[1-9]|[1-9][0-9])",p.id):
+            if not(p.id[0] in regionCodes.keys()):
+                raise Exception("Posible new region found: %s"%p.id)
+            if not(p.id[1] in tractionCodes.keys() or p.id[2] in tractionCodes.keys()):
+                raise Exception("Posible new traction found: %s"%p.id)
+            if not(p.id[4] in addOnCodes.keys()):
+                raise Exception("Posible new Add-On type found: %s"%p.id)
+            if not(p.id[5] in yearsCodes.keys()):
+                raise Exception("Posible new years found: %s"%p.id)
+        else:
+            print("ID with no id style detected: %s"%p.id)
 
 def getProjects():
     global oldProjects,p_dict
@@ -124,38 +166,44 @@ def mergeProjects():
     global date, mergedProjects,newProjects,oldProjects
     mergedProjects = oldProjects.copy()
     IDs = getNewProjectsIDs()
+    remainingIDs = IDs.copy()
     for mProj in mergedProjects:
         if (mProj.id in IDs):
-            mProj.dates.append(newProjects[IDs.index(mProj.id)].dates)
-            mProj.statusA.append(newProjects[IDs.index(mProj.id)].statusA)
-            IDs.remove(mProj.id)
+            mProj.dates.extend(newProjects[IDs.index(mProj.id)].dates)
+            mProj.statusA.extend(newProjects[IDs.index(mProj.id)].statusA)
+            remainingIDs.remove(mProj.id)
         else:
-            if (mProj.statusA[len(mProj.statusA) - 1] == "Upcoming" or mProj.statusA[len(mProj.statusA) - 1] == "Next Arrival"):
+            print("%s is not appearing, last status %s: [R/C]"%(mProj.id,mProj.statusA[len(mProj.statusA) - 1]))
+            uInput = input().upper()
+            
+            if (uInput == "R"):
                 mProj.dates.append(date)
                 mProj.statusA.append("Released")
-            elif (mProj.statusA[len(mProj.statusA) - 1] == "In Production" or mProj.statusA[len(mProj.statusA) - 1] == "In Planning"):
+                print("%s -> Released"%mProj.id)
+            elif (uInput == "C"):
                 mProj.dates.append(date)
                 mProj.statusA.append("Canceled")
+                print("%s -> Canceled"%mProj.id)
 
-    for proj in IDs:
+    for proj in remainingIDs:
         mergedProjects.append(newProjects[IDs.index(proj)])
 
 def createNewRoadmapObjects():
     print("Creating roadmap from data...")
-    global newProjects, newRoadmap, sections
+    global newProjects, newRoadmap, statusCodes
     DATE = date
     NA = []
     UP = []
     PR = []
     PL = []
     for i in newProjects:
-        if i.status == sections[0]:
+        if i.status == statusCodes[0]:
             NA.append(i)
-        elif i.status == sections[1]:
+        elif i.status == statusCodes[1]:
             UP.append(i)
-        elif i.status == sections[2]:
+        elif i.status == statusCodes[2]:
             PR.append(i)
-        elif i.status == sections[3]:
+        elif i.status == statusCodes[3]:
             PL.append(i)
 
     newRoadmap = roadmap(DATE,NA,UP,PR,PL)
@@ -164,11 +212,23 @@ def getRoadmaps():
     print("Creating old roadmap data...")
     global p_dict,oldRoadmaps
     for i in p_dict["Roadmaps"]:
-        DATE = p_dict["Roadmaps"].date
-        NA = p_dict["Roadmaps"].Projects.NextArrival
-        UP = p_dict["Roadmaps"].Projects.Upcoming
-        PR = p_dict["Roadmaps"].Projects.InProduction
-        PL = p_dict["Roadmaps"].Projects.InPlanning
+        DATE = i["date"]
+        NA = []
+        for j in i["Projects"]["NextArrival"]:
+            proj = project(j["id"],j["type"],j["name"],None,None,None)
+            NA.append(proj)
+        UP = []
+        for j in i["Projects"]["Upcoming"]:
+            proj = project(j["id"],j["type"],j["name"],None,None,None)
+            UP.append(proj)
+        PR = []
+        for j in i["Projects"]["InProduction"]:
+            proj = project(j["id"],j["type"],j["name"],None,None,None)
+            PR.append(proj)
+        PL = []
+        for j in i["Projects"]["InPlanning"]:
+            proj = project(j["id"],j["type"],j["name"],None,None,None)
+            PL.append(proj)
         nRoadM = roadmap(DATE,NA,UP,PR,PL)
         oldRoadmaps.append(nRoadM)
 
@@ -176,7 +236,7 @@ def mergeRoadmaps():
     global oldRoadmaps,newRoadmap,mergedRoadmaps
     mergedRoadmaps = [newRoadmap]
     if len(oldRoadmaps) > 0:
-        mergedRoadmaps.append(oldRoadmaps)
+        mergedRoadmaps.extend(oldRoadmaps)
 
 def updateJSON():
     global date,mergedProjects,mergedRoadmaps
@@ -191,94 +251,110 @@ def updateJSON():
     
     #Projects
     file.write('"Projects":[')
+    tmpStr1 = ""
     for proj in mergedProjects:
-        file.write("{")
-        file.write('"id":')
-        file.write('"%s",'%proj.id)
-        file.write('"name":')
-        file.write('"%s",'%proj.name)
-        file.write('"type":')
-        file.write('"%s",'%proj.type)
-        file.write('"timeline":')
-        file.write("{")
-        file.write('"dates":[')
+        tmpStr1 = tmpStr1+"{"
+        tmpStr1 = tmpStr1+'"id":'
+        tmpStr1 = tmpStr1+'"%s",'%proj.id
+        tmpStr1 = tmpStr1+'"name":'
+        tmpStr1 = tmpStr1+'"%s",'%proj.name
+        tmpStr1 = tmpStr1+'"type":'
+        tmpStr1 = tmpStr1+'"%s",'%proj.type
+        tmpStr1 = tmpStr1+'"timeline":'
+        tmpStr1 = tmpStr1+"{"
+        tmpStr1 = tmpStr1+'"dates":['
+        tmpStr2 = ""
         for date in proj.dates:
-            file.write('"%s"'%date)
-            if date != proj.dates[len(proj.dates)-1]:
-                file.write(",")
-        file.write('],"status":[')
+            tmpStr2 = tmpStr2+'"%s"'%date
+            tmpStr2 = tmpStr2+","
+        tmpStr2 = tmpStr2[:-1]
+        tmpStr1 = tmpStr1+tmpStr2
+        tmpStr1 = tmpStr1+'],"status":['
+        tmpStr2 = ""
         for status in proj.statusA:
-            file.write('"%s"'%status)
-            if status != proj.statusA[len(proj.statusA)-1]:
-                file.write(",")
-        file.write("]}}")
-        if proj != mergedProjects[len(mergedProjects)-1]:
-            file.write(",")
+            tmpStr2 = tmpStr2+'"%s"'%status
+            tmpStr2 = tmpStr2+","
+        tmpStr2 = tmpStr2[:-1]
+        tmpStr1 = tmpStr1+tmpStr2
+        tmpStr1 = tmpStr1+"]}}"
+        tmpStr1 = tmpStr1+","
+    tmpStr1 = tmpStr1[:-1]
+    file.write(tmpStr1)
     file.write("],")
     
     #Roadmaps
     file.write('"Roadmaps":[')
+    tmpStr1 = ""
     for road in mergedRoadmaps:
-        file.write("{")
-        file.write('"date":')
-        file.write('"%s",'%road.date)
-        file.write('"Projects":{')
-        file.write('"NextArrival":[')
+        tmpStr1 = tmpStr1+"{"
+        tmpStr1 = tmpStr1+'"date":'
+        tmpStr1 = tmpStr1+'"%s",'%road.date
+        tmpStr1 = tmpStr1+'"Projects":{'
+        tmpStr1 = tmpStr1+'"NextArrival":['
+        tmpStr2 = ""
         for na in road.na:
-            file.write("{")
-            file.write('"id":')
-            file.write('"%s",'%na.id)
-            file.write('"name":')
-            file.write('"%s",'%na.name)
-            file.write('"type":')
-            file.write('"%s"'%na.type)
-            file.write("}")
-            if na != road.na[len(road.na)-1]:
-                file.write(",")
-        file.write("],")
-        file.write('"Upcoming":[')
+            tmpStr2 = tmpStr2+"{"
+            tmpStr2 = tmpStr2+'"id":'
+            tmpStr2 = tmpStr2+'"%s",'%na.id
+            tmpStr2 = tmpStr2+'"name":'
+            tmpStr2 = tmpStr2+'"%s",'%na.name
+            tmpStr2 = tmpStr2+'"type":'
+            tmpStr2 = tmpStr2+'"%s"'%na.type
+            tmpStr2 = tmpStr2+"}"
+            tmpStr2 = tmpStr2+","
+        tmpStr2 = tmpStr2[:-1]
+        tmpStr1 = tmpStr1+tmpStr2
+        tmpStr1 = tmpStr1+"],"
+        tmpStr1 = tmpStr1+'"Upcoming":['
+        tmpStr2 = ""
         for up in road.up:
-            file.write("{")
-            file.write('"id":')
-            file.write('"%s",'%up.id)
-            file.write('"name":')
-            file.write('"%s",'%up.name)
-            file.write('"type":')
-            file.write('"%s"'%up.type)
-            file.write("}")
-            if up != road.up[len(road.up)-1]:
-                file.write(",")
-        file.write("],")
-        file.write('"InProduction":[')
+            tmpStr2 = tmpStr2+"{"
+            tmpStr2 = tmpStr2+'"id":'
+            tmpStr2 = tmpStr2+'"%s",'%up.id
+            tmpStr2 = tmpStr2+'"name":'
+            tmpStr2 = tmpStr2+'"%s",'%up.name
+            tmpStr2 = tmpStr2+'"type":'
+            tmpStr2 = tmpStr2+'"%s"'%up.type
+            tmpStr2 = tmpStr2+"}"
+            tmpStr2 = tmpStr2+","
+        tmpStr2 = tmpStr2[:-1]
+        tmpStr1 = tmpStr1+tmpStr2
+        tmpStr1 = tmpStr1+"],"
+        tmpStr1 = tmpStr1+'"InProduction":['
+        tmpStr2 = ""
         for pr in road.pr:
-            file.write("{")
-            file.write('"id":')
-            file.write('"%s",'%pr.id)
-            file.write('"name":')
-            file.write('"%s",'%pr.name)
-            file.write('"type":')
-            file.write('"%s"'%pr.type)
-            file.write("}")
-            if pr != road.pr[len(road.pr)-1]:
-                file.write(",")
-        file.write("],")
-        file.write('"InPlanning":[')
+            tmpStr2 = tmpStr2+"{"
+            tmpStr2 = tmpStr2+'"id":'
+            tmpStr2 = tmpStr2+'"%s",'%pr.id
+            tmpStr2 = tmpStr2+'"name":'
+            tmpStr2 = tmpStr2+'"%s",'%pr.name
+            tmpStr2 = tmpStr2+'"type":'
+            tmpStr2 = tmpStr2+'"%s"'%pr.type
+            tmpStr2 = tmpStr2+"}"
+            tmpStr2 = tmpStr2+","
+        tmpStr2 = tmpStr2[:-1]
+        tmpStr1 = tmpStr1+tmpStr2
+        tmpStr1 = tmpStr1+"],"
+        tmpStr1 = tmpStr1+'"InPlanning":['
+        tmpStr2 = ""
         for pl in road.pl:
-            file.write("{")
-            file.write('"id":')
-            file.write('"%s",'%pl.id)
-            file.write('"name":')
-            file.write('"%s",'%pl.name)
-            file.write('"type":')
-            file.write('"%s"'%pl.type)
-            file.write("}")
-            if pl != road.pl[len(road.pl)-1]:
-                file.write(",")
-        file.write("]")
-        file.write("}}")
-        if road != mergedRoadmaps[len(mergedRoadmaps)-1]:
-            file.write(",")
-    file.write("]")    
+            tmpStr2 = tmpStr2+"{"
+            tmpStr2 = tmpStr2+'"id":'
+            tmpStr2 = tmpStr2+'"%s",'%pl.id
+            tmpStr2 = tmpStr2+'"name":'
+            tmpStr2 = tmpStr2+'"%s",'%pl.name
+            tmpStr2 = tmpStr2+'"type":'
+            tmpStr2 = tmpStr2+'"%s"'%pl.type
+            tmpStr2 = tmpStr2+"}"
+            tmpStr2 = tmpStr2+","
+        tmpStr2 = tmpStr2[:-1]
+        tmpStr1 = tmpStr1+tmpStr2
+        tmpStr1 = tmpStr1+"]"
+        tmpStr1 = tmpStr1+"}}"
+        tmpStr1 = tmpStr1+","
+    tmpStr1 = tmpStr1[:-1]
+    file.write(tmpStr1)
+    file.write("]")
     file.write("}")
     file.close
 
@@ -291,13 +367,15 @@ def updatedToCurrentJSON():
              
 def main():
     askDate()
-    #JSONtoDict()
+    JSONtoDict()
+    getCodes()
     getRoadmapString()            
     createNewProjectsObjects()
-    #getProjects()
+    checkIdCodes()
+    getProjects()
     mergeProjects()
     createNewRoadmapObjects()
-    #getRoadmaps()
+    getRoadmaps()
     mergeRoadmaps()
     updateJSON()
     updatedToCurrentJSON()
